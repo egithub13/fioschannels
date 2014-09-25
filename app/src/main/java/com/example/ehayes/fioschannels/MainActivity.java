e9c2b554d4d8ce4c0a8f4ecf0501d234fffa3ec5
Eric Hayes <egithub13@gmail.com>
2014-09-25 14:45:16 -0400

    Completed writeable databse and user interface

--- app/src/main/java/com/example/ehayes/fioschannels/MainActivity.java
+++ app/src/main/java/com/example/ehayes/fioschannels/MainActivity.java
@@ -4,23 +4,44 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    EditText name,number;
+    SQLiteAdapter helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        name = (EditText) findViewById(R.id.nameEditText);
+        number = (EditText) findViewById(R.id.numberEditText);
+        helper = new SQLiteAdapter(this);
 
-        Fragment fragment = new FragmentChoice();
-        FragmentManager manager = getFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container,fragment,"FragmentChoice");
-        transaction.commit();
+    }
+
+    public void addChannel(View view){
+        String channelName = name.getText().toString();
+        String channelNumber = number.getText().toString();
+
+        long id = helper.insertData(channelName,channelNumber);
+        if(id<0){
+            Message.message(this,"Unsuccessful");
+
+        }else{
+            Message.message(this,"!!!!!YES!!!!!!!!");
+
+        }
+
+        name.setText("");
+        number.setText("");
     }
 }
--- app/src/main/java/com/example/ehayes/fioschannels/SQLiteAdapter.java
+++ app/src/main/java/com/example/ehayes/fioschannels/SQLiteAdapter.java
@@ -1,5 +1,6 @@
 package com.example.ehayes.fioschannels;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,60 +9,78 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by ehayes on 9/24/2014.
  */
-public class SQLiteAdapter extends SQLiteOpenHelper {
+public class SQLiteAdapter {
 
-    //Channels Table Name
-    private static final String TABLE_CHANNELS = "Channels";
+    SQLiteHelper helper;
+    public SQLiteAdapter(Context context){
 
-    //Channels Table Column Names
-    private static final String UID = "_id";
-    private static final String KEY_CHANNEL_NAME = "Name";
-    private static final String KEY_CHANNEL_NUMBER = "Number";
-
-    private static final String[] COLUMNS = {UID,KEY_CHANNEL_NAME,KEY_CHANNEL_NUMBER};
-
-    //Database Version
-    private static final int DATABASE_VERSION = 1;
-    //Database Name
-    private static final String DATABASE_NAME = "ChannelDB";
-    //Table Name
-    private static final String TABLE_NAME = "CHANNEL TABLE";
-    //Create Channel Table
-    private static final String CREATE_CHANNEL_TABLE = "CREATE TABLE "+ TABLE_NAME + " ( "+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            KEY_CHANNEL_NAME+ " TEXT, "+KEY_CHANNEL_NUMBER+" TEXT )";
-
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS "+ TABLE_NAME;
-    private Context context;
-    public SQLiteAdapter(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
-        Message.message(context,"CONSTRUCTOR CALLED");
+        helper = new SQLiteHelper(context);
+    }
+    public long insertData(String name, String number){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(SQLiteHelper.KEY_CHANNEL_NAME,name);
+        contentValues.put(SQLiteHelper.KEY_CHANNEL_NUMBER,number);
+        long id = db.insert(SQLiteHelper.TABLE_NAME,null,contentValues);
+        return id;
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        //SQL to Create Channel Table
-        try {
-            db.execSQL(CREATE_CHANNEL_TABLE);
-            Message.message(context,"onCreate() Called");
-        } catch (SQLException e) {
-            Message.message(context,""+e);
+    static class SQLiteHelper extends SQLiteOpenHelper {
+
+        //Channels Table Name
+        private static final String TABLE_CHANNELS = "Channels";
+
+        //Channels Table Column Names
+        private static final String UID = "_id";
+        private static final String KEY_CHANNEL_NAME = "Name";
+        private static final String KEY_CHANNEL_NUMBER = "Number";
+
+        private static final String[] COLUMNS = {UID,KEY_CHANNEL_NAME,KEY_CHANNEL_NUMBER};
+
+        //Database Version
+        private static final int DATABASE_VERSION = 3;
+        //Database Name
+        private static final String DATABASE_NAME = "ChannelDB";
+        //Table Name
+        private static final String TABLE_NAME = "CHANNELTABLE";
+        //Create Channel Table
+        private static final String CREATE_CHANNEL_TABLE = "CREATE TABLE "+ TABLE_NAME + " ( " + UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_CHANNEL_NAME + " TEXT, " + KEY_CHANNEL_NUMBER + " TEXT )";
+
+        private static final String DROP_TABLE = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+        private Context context;
+        public SQLiteHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+            this.context = context;
+            Message.message(context,"CONSTRUCTOR CALLED");
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            //SQL to Create Channel Table
+            try {
+                db.execSQL(CREATE_CHANNEL_TABLE);
+                Message.message(context,"onCreate() Called");
+            } catch (SQLException e) {
+                Message.message(context,""+e);
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i2) {
+
+            //Create Fresh Table
+            try {
+                //Drop Older Channel Table if Exists
+                db.execSQL(DROP_TABLE);
+                onCreate(db);
+                Message.message(context,"onUpgrade() Called");
+            } catch (Exception e) {
+                Message.message(context,""+e);
+            }
+
+
         }
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i2) {
-        //Drop Older Channel Table if Exists
-        db.execSQL(DROP_TABLE);
-
-        //Create Fresh Table
-        try {
-            this.onCreate(db);
-            Message.message(context,"onUpgrade() Called");
-        } catch (Exception e) {
-            Message.message(context,""+e);
-        }
-
-
-    }
 }
